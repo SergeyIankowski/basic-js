8b8fb586e02ff6e77d7db87244500c12f8d2cefc
SeregeyIankowski <garret933@gmail.com>
2022-10-15 19:45:10 +0300

    feat: add minesweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const initialMatrix = matrix.map(row => row.map(item => 0));
+  const incrementValues = (x, y, matr) => {
+    if (x < matr.length && y < matr[x].length) {
+      matr[x][y]++;
+      if (x - 1 >= 0) { matr[x - 1][y]++ }; //x-1, y
+      if (x - 1 >= 0 && y - 1) { matr[x - 1][y - 1]++ }; //x-1, y-1
+      if (x + 1 < matr.length) { matr[x + 1][y]++ }; //x+1, y
+      if (x + 1 < matr.length && y + 1 < matr[x].length) { matr[x + 1][y + 1]++ }; //x+1, y+1
+      if (y - 1 >= 0) { matr[x][y - 1]++ }; // x, y-1
+      if (y + 1 < matr[x].length) { matr[x][y + 1]++ }; //x, y+1
+      if (x + 1 < matr.length && y - 1 >= 0) { matr[x + 1][y - 1]++ } //x+1, y-1
+      if (x - 1 >= 0 && y + 1 < matr[x].length) { matr[x - 1][y + 1]++ } // x-1, y+1
+    }
+  }
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (matrix[r][c]) {
+        incrementValues(r, c, initialMatrix);
+        initialMatrix[r][c] = 1;
+      }
+    }
+  }
+  return initialMatrix;
 }
 
 module.exports = {
